77940556b24780527261feae2be6d2a58e0cca7e
Rakesh07778777 <bossrakesh43@gmail.com>
2026-04-01 23:48:37 +0530

    gambling

--- app.js
+++ app.js
@@ -7,4 +7,66 @@
 // 6. Give the user their winnings
 // 7. Play again
 
-  
+const prompt = require('prompt-sync')();
+
+const ROWS = 3;
+const COLUMNS = 3;
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8    
+}
+
+const SYMBOL_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
+const diposit = () => {
+    while(true){
+    const dipositAmount = prompt('Enter your ammount')
+    const numberDipositAmount = parseFloat(dipositAmount)
+    if(isNaN(numberDipositAmount) || numberDipositAmount <= 0){
+        console.log('Please enter valid ammount')
+    }else{
+        return numberDipositAmount
+    }
+    }
+}
+
+const getNumberOfLine = () => {
+    while(true){
+    const lines = prompt('Enter the number of line to bet on (1-3 )')
+    const numberOfLines = parseFloat(lines)
+    if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+        console.log('Invalid number of lines, Please try again')
+    }else{
+        return numberOfLines
+    }
+    }
+}
+
+function gerBet(balance, lines){
+    while(true){
+        const bet = prompt('Enter the bet per line:')
+        const numberOfBet = parseFloat(bet)
+        if(isNaN(numberOfBet) || numberOfBet <= 0 || numberOfBet > balance / lines){
+            console.log('invalid bet, Please try again')
+        }else{
+
+            return numberOfBet
+        }
+    }
+}
+
+const spin = ()=> {
+    
+}
+
+const balance = diposit()
+const numberOfLines = getNumberOfLine()
+const bet = gerBet(balance, numberOfLines)
+console.log(bet)
