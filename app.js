2d4998f861fd7a029814640d3cb80799e48a3186
Rakesh07778777 <bossrakesh43@gmail.com>
2026-04-04 00:21:21 +0530

    get winngings

--- app.js
+++ app.js
@@ -109,6 +109,20 @@ const printRows = (rows) => {
     }
 }
 
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+    for(let row = 0; row < lines; row++){
+        const symbols = rows[row]
+        let allSame = true
+        for(const symbol of symbols){
+            if(symbol != symbols[0]){
+                allSame = false
+                break; 
+            }
+        }
+    }
+}
+
 const balance = diposit()
 const numberOfLines = getNumberOfLine()
 const bet = gerBet(balance, numberOfLines)
